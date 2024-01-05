5694650875f77cae0ff6a13ed9e3029d1bf015f9
LEE JAESOL <70834187+jaesol0105@users.noreply.github.com>
2024-01-05 23:17:13 +0900

    [Silver V] Title: 집합, 코드 정리

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -5,6 +5,34 @@ input = sys.stdin.readline
 m = int(input())
 s = set()
 for _ in range(m):
+  cmd = input().strip().split()
+
+  if len(cmd) == 1:
+    if cmd[0] == 'all':
+      s = set([i for i in range(1,21)])
+    else: # empty
+      s = set()
+
+  else:
+    str, num = cmd[0], cmd[1]
+    num = int(num)
+
+    if str == 'add':
+      s.add(num)
+    elif str == 'remove' and num in s:
+      s.remove(num)
+    elif str == 'check':
+      print(1 if num in s else 0)
+    elif str == 'toggle':
+      if num in s:
+        s.remove(num)
+      else:
+        s.add(num)
+        
+'''
+m = int(input())
+s = set()
+for _ in range(m):
   cmd = input().strip()
   str, num = cmd.split() if ' ' in cmd else (cmd,-1)
   num = int(num)
@@ -24,3 +52,4 @@ for _ in range(m):
     s = set([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
   else: # empty
     s = set()
+'''
