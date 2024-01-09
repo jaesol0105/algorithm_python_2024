f1d7bfefef6d7a8d3c788704d26762c0a1d7d6b7
LEE JAESOL <70834187+jaesol0105@users.noreply.github.com>
2024-01-09 19:20:03 +0900

    [Silver V] Title: 올림픽, 코드 정리

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -3,17 +3,13 @@ import sys
 input = sys.stdin.readline
 
 n, k = map(int,input().split())
-rank = []
-for _ in range(n):
-  nation,g,s,b = map(int,input().split())
-  rank.append([g,s,b,nation])
-
-rank = sorted(rank, reverse=True, key = lambda x:(x[0],x[1],x[2]))
+rank = [list(map(int,input().split())) for _ in range(n)]
+rank = sorted(rank, reverse=True, key = lambda x:(x[1],x[2],x[3]))
 
 tmp_rank = 1
 for i in range(n):
-  if rank[i][3] == k: # 국가번호
+  if rank[i][0] == k: # 국가번호
     print(tmp_rank)
     break
-  if rank[i][0:3] != rank[i+1][0:3]:
-    tmp_rank = i+2
+  if rank[i][1:] != rank[i+1][1:]: # 공동 순위 고려
+    tmp_rank = i+2 # 다음 등수 국가의 순위
