7c27ecd4d8ae2d9bde1df96d7d44881652d7c6c0
LEE JAESOL <70834187+jaesol0105@users.noreply.github.com>
2024-01-24 22:42:24 +0900

    [Silver IV] Title: 스위치 켜고 끄기, 코드 정리

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -4,6 +4,34 @@ import sys
 imput = sys.stdin.readline
 
 N = int(input())
+sw_list = [-1] + list(map(int,input().split()))
+num_of_st = int(input())
+
+for _ in range(num_of_st):
+  sex, sw = map(int, input().split())
+  
+  if sex == 1: #남자
+    for i in range(sw,N+1,sw):
+      sw_list[i] = 0 if sw_list[i] == 1 else 1
+      
+  else: #여자
+    sw_list[sw] = 0 if sw_list[sw] == 1 else 1
+    k = 1
+    while sw-k > 0 and sw+k <= N:
+      if sw_list[sw-k] == sw_list[sw+k]:
+        sw_list[sw-k] = 0 if sw_list[sw-k] == 1 else 1
+        sw_list[sw+k] = 0 if sw_list[sw+k] == 1 else 1
+        k += 1
+      else:
+        break
+
+for i in range(1,N+1):
+  print(sw_list[i], end=' ')
+  if i % 20 == 0 :
+    print()
+    
+'''
+N = int(input())
 sw_list = list(map(int,input().split()))
 num_of_st = int(input())
 
@@ -33,3 +61,4 @@ for i in range(1,N+1):
     print(sw_list[i-1])
   else:
     print(sw_list[i-1], end=' ')
+'''
