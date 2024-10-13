548fb9a529629a0f23c397cab2e5f432c8cdfb95
LEE JAESOL <70834187+jaesol0105@users.noreply.github.com>
2024-10-13 12:24:23 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -29,36 +29,3 @@ for i in range(1,N+1):
   print(sw_list[i], end=' ')
   if i % 20 == 0 :
     print()
-    
-'''
-N = int(input())
-sw_list = list(map(int,input().split()))
-num_of_st = int(input())
-
-for i in range(num_of_st):
-  sex, sw = input().strip().split()
-  sex, sw = int(sex), int(sw)
-  
-  if sex == 1: #남자
-    origin_sw = sw
-    while sw <= N:
-      sw_list[sw-1] = 0 if sw_list[sw-1] == 1 else 1
-      sw += origin_sw
-      
-  else: #여자
-    sw_list[sw-1] = 0 if sw_list[sw-1] == 1 else 1
-    neighbor = 1
-    while sw-neighbor > 0 and sw+neighbor <= N:
-      if sw_list[sw-neighbor-1] == sw_list[sw+neighbor-1]:
-        sw_list[sw-neighbor-1] = 0 if sw_list[sw-neighbor-1] == 1 else 1
-        sw_list[sw+neighbor-1] = 0 if sw_list[sw+neighbor-1] == 1 else 1
-        neighbor += 1
-      else:
-        break
-
-for i in range(1,N+1):
-  if i % 20 == 0 :
-    print(sw_list[i-1])
-  else:
-    print(sw_list[i-1], end=' ')
-'''
