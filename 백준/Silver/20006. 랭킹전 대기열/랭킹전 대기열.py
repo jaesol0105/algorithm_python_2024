ae33458f65ecc135bc5bd793ad0dd7619ae2caab
LEE JAESOL <70834187+jaesol0105@users.noreply.github.com>
2024-11-03 20:49:13 +0900

    [Silver II] Title: 랭킹전 대기열, Time: 100 ms, Memory: 111400 KB -BaekjoonHub

---
+++ 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.py
@@ -0,0 +1,21 @@
+p, m = list(map(int,input().split()))
+q = []
+for _ in range(p):
+    level, nick = list(input().split())
+    level = int(level)
+    check = False
+    for game in q:
+        if game[0][0]-10 <= level <= game[0][0]+10 and len(game) < m:
+            game.append((level,nick))
+            check = True
+            break
+    if not check:
+        q.append([(level,nick)])
+
+for game in q:
+    if len(game) == m:
+        print('Started!')
+    else:
+        print('Waiting!')
+    for level, nick in sorted(game, key= lambda x:x[1]):
+        print(level, nick)
