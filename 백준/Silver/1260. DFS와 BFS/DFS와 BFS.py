4ca52d3160cb8a89ecab639ec780f4f0ae8e2766
LEE JAESOL <70834187+jaesol0105@users.noreply.github.com>
2024-11-06 09:25:23 +0900

    [Silver II] Title: DFS와 BFS, Time: 48 ms, Memory: 32236 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,19 +1,18 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
 
 def dfs(v1):
     print(v1, end=' ')
     visited[v1] = 1
-    for v2 in sorted(graph[v1]):
+    for v2 in graph[v1]:
         if visited[v2] == 0:
             dfs(v2)
 
 def bfs():
     while q:
-        v1 = q.popleft()
+        v1 = q.pop(0)
         print(v1, end=' ')
-        for v2 in sorted(graph[v1]):
+        for v2 in graph[v1]:
             if visited[v2] == 0:
                 visited[v2] = 1 #위치
                 q.append(v2)
@@ -24,14 +23,15 @@ for _ in range(M):
     a,b = list(map(int, input().split()))
     graph[a].append(b)
     graph[b].append(a) #양방향
+for i in range(N+1):
+    graph[i].sort()
 
-q = deque([V])
 visited = [0] * (N+1)
 dfs(V)
 
 print('')
 
-q = deque([V])
+q = [V]
 visited = [0] * (N+1)
 visited[V] = 1
 bfs()
