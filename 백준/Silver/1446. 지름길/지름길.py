79e6f92023afe2a0114b69621609d8524f53b57a
LEE JAESOL <70834187+jaesol0105@users.noreply.github.com>
2025-08-20 08:48:46 +0900

    [Silver I] Title: 지름길, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.py
+++ 백준/Silver/1446. 지름길/지름길.py
@@ -6,7 +6,7 @@ for i in range(d+1):
     if i > 0:
         ans[i]= min(ans[i], ans[i-1]+1)
     for sta, end, distance in rlist:
-        if sta == i and end <= d: #지름길이있고, 목적지앞까지면
-            ans[end]= min(ans[i]+distance, ans[end])
+        if sta == i and end <= d and ans[end] > ans[i]+distance:
+            ans[end]= ans[i]+distance
 
 print(ans[d])
